a78b39364c7f574ad1111680487d0d60a43aa40c
yvvlt <yewon5260@naver.com>
2026-04-13 13:36:48 +0900

    Update ex6.py

--- 6주차/ex6.py
+++ 6주차/ex6.py
@@ -1,3 +1,20 @@
+## 클래스의 함수 선언 부분 ##
+
+class Node() :              #노드생성
+    def __init__(self) :
+        self.data = None
+        self.link = None
+
+def printNodes(start) :     #현재 노드 데이터 출력
+    current = start         #(head 부터 노드의 끝까지)
+    if current == None :
+        return
+    print(current.data, end=' ')
+    while current.link != None :
+        current = current.link
+        print(current.data, end=' ')
+    print()
+
 def insertNode(findData, insertData) :
     global memory, head, current, pre
 
